f214ab8133478bcc5df17ba1d7aa9036f22433c3
fransosiche <francois.biron@viacesi.fr>
2018-05-28 09:12:12 +0200

    Commit 1, delete initial project

--- lorann/main/src/main/java/main/Main.java
+++ lorann/main/src/main/java/main/Main.java
@@ -14,20 +14,13 @@ import view.ViewFacade;
  */
 public abstract class Main {
 
-    /**
+	/**
      * The main method.
      *
      * @param args
      *            the arguments
      */
     public static void main(final String[] args) {
-        final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
-
-        try {
-            controller.start();
-        } catch (final SQLException exception) {
-            exception.printStackTrace();
-        }
-    }
+        
 
 }
