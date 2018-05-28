1d7f7768ebb0346d3bd11b11cf97614ef3c23200
arcimed <julien.fallempinpin@orange.fr>
2018-05-28 11:01:45 +0200

    update site

--- lorann/main/src/main/java/main/Main.java
+++ lorann/main/src/main/java/main/Main.java
@@ -22,7 +22,7 @@ public abstract class Main {
      */
     public static void main(final String[] args) {
         System.out.println("hello");
-        System.out.println("Franso");
+        System.out.println("Frano");
 
 }
 }
